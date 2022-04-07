5a112b4d6246513dbf61044059083121e0ac21c9
Fardeen Khan <khanfardeen9983@gmail.com>
2022-04-07 16:03:26 +0530

    Sorting layers, Enemy Rows added

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -24,10 +24,16 @@ public class SpawnEnemy : MonoBehaviour
         enemyRows.ForEach((row) =>
         {
             Vector3 position = row.transform.position;
+            Vector3 startPoint = position, endPoint = new Vector3(-1 * position.x, position.y, position.z);
+            
             GameObject enemyType = EnemyTypes[Random.Range(0, EnemyTypes.Count)];
-            for(int i = 0; i < SpawnCount; i++)
+            Size enemySize = Utils.getSizeByCollider(enemyType);
+            int Count = (int)(Mathf.Abs(position.x) * 2 / (enemySize.Width));
+            for (int i = 0; i < Count; i++)
             {
-                Instantiate(enemyType, position,row.transform.rotation);
+                var enemy = Instantiate(enemyType, position,row.transform.rotation);
+                enemy.transform.parent = row.transform;
+                position = new Vector3(position.x + enemySize.Width * 1.15f, position.y, position.z);
             }
             
         });
@@ -37,6 +43,9 @@ public class SpawnEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        enemyRows.ForEach(row =>
+        {
+            row.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -0.25f);
+        });
     }
 }
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Utils
+{
+   public static Size getSizeByCollider(GameObject gameObject)
+    {
+        BoxCollider2D collider = gameObject.GetComponent<BoxCollider2D>();
+        
+        return new Size
+        {
+            Width = collider.bounds.size.x,
+            Height = collider.bounds.size.y
+        };
+    }
+}
+
+public class Size
+{
+    public float Width { get; set; }
+    public float Height { get; set; }
+}
+
